60654b0b5db665ba18e3534f6a00f5e5dcead768
Aleš Roubíček <ales@roubicek.name>
2022-04-20 12:37:43 +0200

    Added private bucket for hacker profiles

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import {
   createGoogleMxRecords,
   createTxtRecord,
   Website,
+  CloudFront,
 } from "@topmonks/pulumi-aws";
 
 registerAutoTags({
@@ -28,8 +29,75 @@ createTxtRecord(
   "google-site-verification=eIaBVqhznPV-0AAEEbFJN82j3w063w_tW0-DUZWX5C0"
 );
 
-export const websites: any = {
-  [donutDomain]: siteExports(Website.create(donutDomain, {})),
+const hackersOai = new aws.cloudfront.OriginAccessIdentity(
+  "hc-hacker-profiles",
+  {
+    comment: "Hackers profiles access identity",
+  }
+);
+
+const hackerProfilesBucket = new aws.s3.Bucket("hc-hacker-profiles", {
+  acl: "private",
+  bucketPrefix: "hc-hacker-profiles",
+  forceDestroy: true,
+});
+
+const hackerProfilesBucketPolicy = new aws.s3.BucketPolicy(
+  "hc-hacker-profiles",
+  {
+    bucket: hackerProfilesBucket.bucket,
+    policy: JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Sid: "1",
+          Effect: "Allow",
+          Principal: {
+            AWS: hackersOai.s3CanonicalUserId,
+          },
+          Action: "s3:GetObject",
+          Resource: `arn:aws:s3:::${hackerProfilesBucket.bucketDomainName}/*`,
+        },
+      ],
+    }),
+  }
+);
+
+export const websites: Record<string, WebsiteExport> = {
+  [donutDomain]: siteExports(
+    Website.create(donutDomain, {
+      extraOrigins: [
+        {
+          originId: "hackerProfiles",
+          domainName: hackerProfilesBucket.bucketRegionalDomainName,
+          s3OriginConfig: {
+            originAccessIdentity: hackersOai.cloudfrontAccessIdentityPath,
+          },
+          customOriginConfig: {
+            originProtocolPolicy: "http-only",
+            httpPort: 80,
+            httpsPort: 443,
+            originSslProtocols: ["TLSv1.2"],
+          },
+        },
+      ],
+      extraCacheBehaviors: [
+        {
+          pathPattern: "hackers/*",
+          targetOriginId: hackerProfilesBucket.arn,
+          allowedMethods: ["GET", "HEAD", "OPTIONS"],
+          cachedMethods: ["GET", "HEAD"],
+          viewerProtocolPolicy: "redirect-to-https",
+          compress: true,
+          cachePolicyId: CloudFront.ManagedCachePolicy.CachingOptimized,
+          responseHeadersPolicyId:
+            CloudFront.ManagedResponseHeaderPolicy.SecurityHeadersPolicy,
+          originRequestPolicyId:
+            CloudFront.ManagedOriginRequestPolicy.AllViewer,
+        },
+      ],
+    })
+  ),
   [webDomain]: siteExports(Website.create(webDomain, {})),
 };
 
