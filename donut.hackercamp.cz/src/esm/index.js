b7b01b7c7a2335fc3bd6511991142c221bc4c774
Aleš Roubíček <ales@roubicek.name>
2022-04-24 13:37:41 +0200

    Store tokens and profile in local storage

--- donut.hackercamp.cz/src/esm/index.js
+++ donut.hackercamp.cz/src/esm/index.js
@@ -1,19 +1,24 @@
 export async function main({ searchParams, slackButton, env }) {
+  const apiURL = (endpoint) => new URL(endpoint, env["api-host"]).href;
+
   if (localStorage.getItem("hc:id_token")) {
     slackButton.style.display = "none";
   }
+
   if (searchParams.has("code")) {
     const code = searchParams.get("code");
-    const resp = await fetch(new URL("auth", env["api-host"]), {
+    const resp = await fetch(apiURL("auth"), {
       method: "POST",
       body: new URLSearchParams({ code }),
     });
     const data = await resp.json();
     if (resp.ok && data.ok) {
-      const { idToken } = data;
+      const { idToken, slackToken, slackProfile } = data;
       localStorage.setItem("hc:id_token", idToken);
+      localStorage.setItem("slack:id_token", slackToken);
+      localStorage.setItem("slack:profile", JSON.stringify(slackToken));
       slackButton.style.display = "none";
-      console.log(data);
+      console.log(slackProfile);
     } else {
       console.error(data);
     }
