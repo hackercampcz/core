d549d9b2e80b12ecd7a11013da86db68553d19a2
Aleš Roubíček <ales@roubicek.name>
2022-05-22 10:03:33 +0200

    [api.hackercamp.cz] Back to cjs format

--- lambda-builder.ts
+++ lambda-builder.ts
@@ -1,23 +1,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import { buildSync } from "esbuild";
 
-const REQUIRE_SHIM = `import module from 'module';
-if (typeof globalThis.require == "undefined") globalThis.require = module.createRequire(import.meta.url);`;
-
 function build(entrypoint: string, minify: boolean) {
   const result = buildSync({
     bundle: true,
     minify,
-    format: "esm",
+    format: "cjs",
     charset: "utf8",
     platform: "node",
     target: "node14.8",
     external: ["aws-sdk"],
     entryPoints: [entrypoint],
     write: false,
-    banner: {
-      js: REQUIRE_SHIM,
-    },
   });
   return result?.outputFiles?.[0].text ?? "";
 }
@@ -27,6 +21,6 @@ export function buildCodeAsset(
   minify = false
 ): pulumi.asset.AssetArchive {
   return new pulumi.asset.AssetArchive({
-    "index.mjs": new pulumi.asset.StringAsset(build(entrypoint, minify)),
+    "index.js": new pulumi.asset.StringAsset(build(entrypoint, minify)),
   });
 }
