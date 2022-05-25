e29a051b31129253802cf7711e921e345dd4573e
Aleš Roubíček <ales@roubicek.name>
2022-05-25 17:40:03 +0200

    [api] get registration data by id

--- api.hackercamp.cz/src/lambda/registration/get.mjs
+++ api.hackercamp.cz/src/lambda/registration/get.mjs
@@ -1,6 +1,6 @@
-import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
-import { unmarshall } from "@aws-sdk/util-dynamodb";
-import { response, internalError, withCORS } from "../http.mjs";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { response, internalError, withCORS, notFound } from "../http.mjs";
 
 /** @typedef { import("@aws-sdk/client-dynamodb").DynamoDBClient } DynamoDBClient */
 /** @typedef { import("@pulumi/awsx/apigateway").Request } APIGatewayProxyEvent */
@@ -15,9 +15,20 @@ const db = new DynamoDBClient({});
  */
 export async function handler(event) {
   const withCORS_ = withCORS(["POST", "OPTIONS"], event.headers["origin"]);
+  const { id } = event.queryStringParameters;
   try {
-    console.log(event);
-    return withCORS_(response("OK"));
+    const resp = await db.send(
+      new ScanCommand({
+        TableName: "hc-registrations",
+        Select: "ALL_ATTRIBUTES",
+        Limit: 1,
+        FilterExpression: "id = :id",
+        ExpressionAttributeValues: marshall({ ":id": id }),
+      })
+    );
+    const data = resp.Items.map((x) => unmarshall(x));
+    if (!data.length) return withCORS_(notFound());
+    return withCORS_(response(data));
   } catch (err) {
     console.error(err);
     return withCORS_(internalError());
