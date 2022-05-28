a6228b0eaad6f668cfe4dc585870ea1af7488f30
Aleš Roubíček <ales@roubicek.name>
2022-05-28 10:39:30 +0200

    [api] Updated registration data for donut

--- api.hackercamp.cz/src/lambda/registration/get.mjs
+++ api.hackercamp.cz/src/lambda/registration/get.mjs
@@ -1,4 +1,8 @@
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { response, internalError, withCORS, notFound } from "../http.mjs";
 
@@ -9,6 +13,52 @@ import { response, internalError, withCORS, notFound } from "../http.mjs";
 /** @type DynamoDBClient */
 const db = new DynamoDBClient({});
 
+async function getRegistrationById(id) {
+  const resp = await db.send(
+    new ScanCommand({
+      TableName: "hc-registrations",
+      Select: "ALL_ATTRIBUTES",
+      Limit: 1,
+      FilterExpression: "id = :id",
+      ExpressionAttributeValues: marshall({ ":id": id }),
+    })
+  );
+  const [data] = resp.Items.map((x) => unmarshall(x));
+  return data;
+}
+
+async function getRegistrationByEmail(email, year, slackID) {
+  const [contactResp, regResp] = await Promise.all([
+    db.send(
+      new GetItemCommand({
+        TableName: "hc-contacts",
+        Key: { email, slackID },
+      })
+    ),
+    db.send(
+      new GetItemCommand({
+        TableName: "hc-registrations",
+        Key: { email, year },
+      })
+    ),
+  ]);
+
+  if (regResp.Item) {
+    return unmarshall(regResp.Item);
+  }
+  if (contactResp.Item) {
+    const contact =  unmarshall(contactResp.Item);
+    return {
+      invRegNo: contact.companyID,
+      invVatNo: contact.vatID,
+      invAddress: contact.address,
+      invEmail: contact.email,
+      invName: contact.company || contact.name,
+    };
+  }
+  return null;
+}
+
 /**
  * @param {APIGatewayProxyEvent} event
  * @returns {Promise.<APIGatewayProxyResult>}
@@ -18,19 +68,12 @@ export async function handler(event) {
     ["GET", "POST", "OPTIONS"],
     event.headers["origin"]
   );
-  const { id } = event.queryStringParameters;
+  const { id, email, year, slackID } = event.queryStringParameters;
   try {
-    const resp = await db.send(
-      new ScanCommand({
-        TableName: "hc-registrations",
-        Select: "ALL_ATTRIBUTES",
-        Limit: 1,
-        FilterExpression: "id = :id",
-        ExpressionAttributeValues: marshall({ ":id": id }),
-      })
-    );
-    const data = resp.Items.map((x) => unmarshall(x));
-    if (!data.length) return withCORS_(notFound());
+    const data = id
+      ? await getRegistrationById(id)
+      : await getRegistrationByEmail(email, year, slackID);
+    if (!data) return withCORS_(notFound());
     return withCORS_(response(data));
   } catch (err) {
     console.error(err);
