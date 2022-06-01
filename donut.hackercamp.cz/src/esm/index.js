8ed3a5c2c9a585025d639d902c7352b816085a3e
Aleš Roubíček <ales@roubicek.name>
2022-06-01 08:35:19 +0200

    [donut] fix darkmode, improve HP

--- donut.hackercamp.cz/src/esm/index.js
+++ donut.hackercamp.cz/src/esm/index.js
@@ -1,5 +1,19 @@
-function hideSlackButton(slackButton) {
-  slackButton.style.display = "none";
+import { render, html } from "lit-html";
+
+function hideSlackButton(slackButton, slackProfile) {
+  slackButton.innerHTML = "";
+  const profileLink = `https://app.slack.com/client/T01V4Q0ACQ4/C01V4Q0AZ0U/user_profile/${slackProfile.sub}`;
+  render(
+    html`
+      <p>
+        Brzy tady najdeš svůj profil, výběr ubytování, program a seznamovací
+        hru. Mezi tím si
+        <a href="${profileLink}">doplň svůj profil na Slacku</a> nebo
+        <a href="/registrace/">uprav svoji registraci</a>.
+      </p>
+    `,
+    slackButton
+  );
 }
 
 async function authenticate({ searchParams, apiURL }) {
@@ -28,6 +42,13 @@ function handleReturnUrl() {
   location.assign(returnUrl);
 }
 
+function signOut() {
+  localStorage.removeItem("hc:id_token");
+  localStorage.removeItem("slack:id_token");
+  localStorage.removeItem("slack:profile");
+  window.dispatchEvent(new Event("hc:profile"));
+}
+
 export async function main({ searchParams, slackButton, env }) {
   const apiURL = (endpoint) => new URL(endpoint, env["api-host"]).href;
 
@@ -35,14 +56,14 @@ export async function main({ searchParams, slackButton, env }) {
     searchParams.has("returnUrl") &&
     searchParams.get("state") === "not-authenticated"
   ) {
-    localStorage.removeItem("hc:id_token");
-    localStorage.removeItem("slack:id_token");
-    localStorage.removeItem("slack:profile");
-    window.dispatchEvent(new Event("hc:profile"));
+    signOut();
   }
 
   if (localStorage.getItem("hc:id_token")) {
-    hideSlackButton(slackButton);
+    hideSlackButton(
+      slackButton,
+      JSON.parse(localStorage.getItem("slack:profile"))
+    );
   }
 
   if (searchParams.has("returnUrl")) {
@@ -51,9 +72,9 @@ export async function main({ searchParams, slackButton, env }) {
 
   if (searchParams.has("code")) {
     try {
-      await authenticate({ searchParams, apiURL });
+      const slackProfile = await authenticate({ searchParams, apiURL });
       window.dispatchEvent(new Event("hc:profile"));
-      hideSlackButton(slackButton);
+      hideSlackButton(slackButton, slackProfile);
       handleReturnUrl();
     } catch (e) {
       console.error(e);
