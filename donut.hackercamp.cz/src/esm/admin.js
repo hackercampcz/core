c6029ed16d5b5d2957d18f5f8ea1697a71ef63ea
Aleš Roubíček <ales@roubicek.name>
2022-06-06 16:24:57 +0200

    [donut] Admin more details

--- donut.hackercamp.cz/src/esm/admin.js
+++ donut.hackercamp.cz/src/esm/admin.js
@@ -1,5 +1,5 @@
 import { defAtom } from "@thi.ng/atom";
-import { html } from "lit-html";
+import { html, svg } from "lit-html";
 import { classMap } from "lit-html/directives/class-map.js";
 import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
 import { until } from "lit-html/directives/until.js";
@@ -218,10 +218,157 @@ function chips(view, { hackers, waitingList, plusOnes, hackersConfirmed }) {
   </span>`;
 }
 
+const housing = new Map([
+  ["own-car", "Přijede autem a bude v něm i spát"],
+  ["own-caravan", "Přiveze si vlastní karavan, ve kterém chce spát"],
+  ["open-air", "Bude spát pod širákem nebo v hamace"],
+  ["own-tent", "Bude spát ve stanu"],
+  ["glamping", "Bude spát v Glamping stanu"],
+  ["cottage", "Bude spát v chatce"],
+  ["nearby", "Využije možnost ubytování v okolí"],
+]);
+
+const travel = new Map([
+  ["full-car", "Přijede autem a jsou full"],
+  ["free-car", "Přijede autem a nabídne spolujízdu"],
+  ["carpool", "S někým  se sveze"],
+  ["bus", "Chce jet Hacker busem z Brna a okolí!"],
+  ["no-car", "Nejede autem (využije bus, vlak, kolo, pěškobus)"],
+  ["no-car-but-info", "Nejedu autem, ale zajímá ho shuttlebus"],
+]);
+
+const ticketBadge = new Map([
+  [
+    "nonprofit",
+    svg`
+      <svg
+        class="hc-price-list__badge"
+        width="24"
+        height="24"
+        viewBox="0 0 66 66"
+        fill="none"
+         xmlns="http://www.w3.org/2000/svg">
+        <title>Táborník z neziskovky</title>
+        <g clip-path="url(#item1_clip0)">
+          <rect width="66" height="66" rx="20" fill="url(#paint5_linear)" />
+          <circle opacity="0.23" cx="52" cy="11" r="17" fill="white" />
+          <circle opacity="0.23" cx="11" cy="55" r="24" fill="white" />
+        </g>
+        <defs>
+          <linearGradient id="paint5_linear" x1="2.55465e-06" y1="-8" x2="66" y2="73.5"
+                          gradientUnits="userSpaceOnUse">
+            <stop stop-color="#E0E0E0" />
+            <stop offset="1" stop-color="#BEBEBE" />
+          </linearGradient>
+          <clipPath id="item1_clip0">
+            <rect width="66" height="66" rx="20" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    `,
+  ],
+  [
+    "hacker",
+    svg`
+      <svg
+        class="hc-price-list__badge"
+        width="24"
+        height="24"
+        viewBox="0 0 66 66"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <title>Hacker</title>
+        <g clip-path="url(#item2_clip0)">
+          <rect width="66" height="66" rx="20" fill="url(#paint6_linear)" />
+          <circle opacity="0.23" cx="52" cy="11" r="17" fill="url(#paint1_linear)" />
+          <circle opacity="0.23" cx="11" cy="55" r="24" fill="url(#paint2_linear)" />
+        </g>
+        <defs>
+          <linearGradient id="paint6_linear" x1="2.55465e-06" y1="-8" x2="66" y2="73.5"
+                          gradientUnits="userSpaceOnUse">
+            <stop stop-color="#E0E0E0" />
+            <stop offset="1" stop-color="#BEBEBE" />
+          </linearGradient>
+          <linearGradient id="paint1_linear" x1="33.0682" y1="21.0455" x2="85.2184" y2="11.0029"
+                          gradientUnits="userSpaceOnUse">
+            <stop stop-color="#EE771B" />
+            <stop offset="0.4271" stop-color="#E62271" />
+            <stop offset="0.7889" stop-color="#684997" />
+            <stop offset="1" stop-color="#3E7ABC" />
+          </linearGradient>
+          <linearGradient id="paint2_linear" x1="-15.7273" y1="69.1818" x2="57.8966" y2="55.0041"
+                          gradientUnits="userSpaceOnUse">
+            <stop stop-color="#EE771B" />
+            <stop offset="0.4271" stop-color="#E62271" />
+            <stop offset="0.7889" stop-color="#684997" />
+            <stop offset="1" stop-color="#3E7ABC" />
+          </linearGradient>
+          <clipPath id="item2_clip0">
+            <rect width="66" height="66" rx="20" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    `,
+  ],
+  [
+    "hacker-plus",
+    svg`
+      <svg
+        class="hc-price-list__badge"
+        width="24" height="24" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <title>Hacker filantrop</title>
+        <g clip-path="url(#item3_clip0)">
+          <rect width="66" height="66" rx="20" fill="url(#paint3_linear)" />
+          <circle opacity="0.23" cx="52" cy="11" r="17" fill="white" />
+          <circle opacity="0.23" cx="11" cy="55" r="24" fill="white" />
+        </g>
+        <defs>
+          <linearGradient id="paint3_linear" x1="-3.75" y1="52.5" x2="97.4828" y2="33.0057"
+                          gradientUnits="userSpaceOnUse">
+            <stop stop-color="#EE771B" />
+            <stop offset="0.4271" stop-color="#E62271" />
+            <stop offset="0.7889" stop-color="#684997" />
+            <stop offset="1" stop-color="#3E7ABC" />
+          </linearGradient>
+          <clipPath id="item3_clip0">
+            <rect width="66" height="66" rx="20" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    `,
+  ],
+  [
+    "hacker-patron",
+    svg`
+      <svg
+        class="hc-price-list__badge"
+        width="24" height="24" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <title>Patron Campu</title>
+        <g clip-path="url(#item4_clip0)">
+          <rect width="66" height="66" rx="20" fill="url(#paint10_linear)" />
+          <circle opacity="0.23" cx="52" cy="11" r="17" fill="white" />
+          <circle opacity="0.23" cx="11" cy="55" r="24" fill="white" />
+        </g>
+        <defs>
+          <linearGradient id="paint10_linear" x1="-3.75" y1="52.5" x2="97.4828" y2="33.0057"
+                          gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FDF59A" />
+            <stop offset="0.498264" stop-color="#F1CF5D" />
+            <stop offset="1" stop-color="#E1BB49" />
+          </linearGradient>
+          <clipPath id="item4_clip0">
+            <rect width="66" height="66" rx="20" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    `,
+  ],
+]);
+
 function detailTemplate(detail) {
   if (!detail) return null;
   return html`
-    <div class="hc-card" style="min-width: 33%">
+    <div class="hc-card hc-master-detail__detail"">
       <h2>${detail.firstName} ${detail.lastName}</h2>
       <p>${detail.company}</p>
       <div class="hc-detail__tools">
@@ -234,6 +381,7 @@ function detailTemplate(detail) {
       >
         <path d="M0 0h24v24H0z" fill="none"/>
         <path
+          fill="var(--hc-text-color)"
           d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"
         />
       </svg></a>
@@ -246,10 +394,23 @@ function detailTemplate(detail) {
         >
           <path d="M0 0h24v24H0z" fill="none"/>
           <path
+            fill="var(--hc-text-color)"
             d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 0 0-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"
           />
         </svg></a>
+
+        ${ticketBadge.get(detail.ticketType)}
       </div>
+      ${when(
+        detail.inviter,
+        () => html`<p>Pozval ho <strong>${detail.inviter}</strong></p>`
+      )}
+      <p>Ubytování: <strong>${
+        housing.get(detail.housing) ?? "Ještě si nevybral"
+      }</strong></p>
+      <p>Doprava: <strong>${
+        travel.get(detail.travel) ?? "Ještě si nevybral"
+      }</strong></p>
       ${when(
         detail.address,
         () => html`
