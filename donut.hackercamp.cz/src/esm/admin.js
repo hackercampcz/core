ae97e361e0b147ec8d86fc26fe9d5b47ff80c5a3
Aleš Roubíček <ales@roubicek.name>
2022-06-06 16:46:53 +0200

    [donut] Admin fine tuning

--- donut.hackercamp.cz/src/esm/admin.js
+++ donut.hackercamp.cz/src/esm/admin.js
@@ -369,7 +369,9 @@ function detailTemplate(detail) {
   if (!detail) return null;
   return html`
     <div class="hc-card hc-master-detail__detail"">
-      <h2>${detail.firstName} ${detail.lastName}</h2>
+      <h2 style="display: flex;align-items: center;gap: 12px;">
+        <span>${detail.firstName} ${detail.lastName}</span>
+        ${ticketBadge.get(detail.ticketType)}</h2>
       <p>${detail.company}</p>
       <div class="hc-detail__tools">
         <a
@@ -398,8 +400,6 @@ function detailTemplate(detail) {
             d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 0 0-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"
           />
         </svg></a>
-
-        ${ticketBadge.get(detail.ticketType)}
       </div>
       ${when(
         detail.inviter,
