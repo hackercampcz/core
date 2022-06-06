297a69f296f9106026dfed7abe3bd3861b6fc4ab
Aleš Roubíček <ales@roubicek.name>
2022-06-06 15:30:54 +0200

    [donut] Admin - markdown for activity, dark-mode

--- donut.hackercamp.cz/src/esm/admin.js
+++ donut.hackercamp.cz/src/esm/admin.js
@@ -1,9 +1,11 @@
 import { defAtom } from "@thi.ng/atom";
 import { html } from "lit-html";
 import { classMap } from "lit-html/directives/class-map.js";
+import { unsafeHTML } from "lit-html/directives/unsafe-html.js";
 import { until } from "lit-html/directives/until.js";
 import { when } from "lit-html/directives/when.js";
 import { initRenderLoop } from "./renderer.js";
+import * as marked from "marked";
 
 const View = {
   hackers: "hackers",
@@ -260,7 +262,7 @@ function detailTemplate(detail) {
         detail.activity,
         () => html`
           <h3>Aktivita</h3>
-          <p>${detail.activity}</p>
+          ${unsafeHTML(marked.parse(detail.activity))}
           <p>${detail.activityCrew}</p>
           <p>${detail.activityPlace}</p>
         `
@@ -309,6 +311,7 @@ function renderTable(data, { view, detail }) {
                     >
                       <path d="M0 0h24v24H0z" fill="none"/>
                       <path
+                        fill="var(--hc-text-color)"
                         d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"
                       />
                     </svg></a>
@@ -321,6 +324,7 @@ function renderTable(data, { view, detail }) {
                       >
                         <path d="M0 0h24v24H0z" fill="none"/>
                         <path
+                          fill="var(--hc-text-color)"
                           d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56a.977.977 0 0 0-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z"
                         />
                       </svg></a>
