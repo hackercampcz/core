6f3779370b309ef3b756a5b6f4a9bde0e6466570
Daniel Hromada <hromada.dan@gmail.com>
2022-06-27 11:03:02 +0200

    #104 render static data of variants and types
    and fix mocks

--- donut.hackercamp.cz/src/esm/housing.js
+++ donut.hackercamp.cz/src/esm/housing.js
@@ -3,20 +3,31 @@
 async function loadHousingData() {
   const responses = await Promise.all([
     fetch(`/housing/index.json`),
+    fetch(`/housing/types.json`),
+    fetch(`/housing/variants.json`),
     fetch(`/housing/hackers.json`),
   ]);
-  const [housing, hackers] = await Promise.all(responses.map((r) => r.json()));
-  return { housing, hackers };
+  const [housing, types, variants, hackers] = await Promise.all(
+    responses.map((r) => r.json())
+  );
+  return { housing, types, variants, hackers };
 }
 
-function watchHousingTypeSwitch({ selectElement, sectionElements }) {
+function renderHousingTypes(selectElement, { types, formElement }) {
+  for (const type of types) {
+    const option = document.createElement("option");
+    option.value = type.name;
+    option.textContent = type.title;
+    selectElement.appendChild(option);
+  }
   selectElement.addEventListener("change", (event) => {
     const { value } = event.target;
-    for (let sectionElm of sectionElements) {
-      if (Array.from(sectionElm.classList).includes(`${value}-housing`)) {
-        sectionElm.removeAttribute("aria-hidden");
-      } else {
-        sectionElm.setAttribute("aria-hidden", "true");
+
+    for (let section of formElement.querySelectorAll("section")) {
+      if (section.classList.contains(`${value}-housing`)) {
+        section.setAttribute("aria-hidden", "false");
+      } else if (!section.classList.contains("housing-type")) {
+        section.setAttribute("aria-hidden", "true");
       }
     }
   });
@@ -54,12 +65,31 @@ function renderHousing() {
   `;
 }
 
-export async function main({ formElement, sectionElements }) {
-  const selectElement = formElement.elements.type;
-  watchHousingTypeSwitch({ selectElement, sectionElements });
+function renderHousingVariants(rootElement, { variants, housing }) {
+  for (const variant of variants) {
+    const sectionElement = document.createElement("section");
+    sectionElement.classList.add(`${variant.type}-housing`);
+    sectionElement.setAttribute("aria-hidden", "true");
+    sectionElement.innerHTML = `
+      <h2>${variant.title}</h2>
+      <div class="hc-card">
+        <p>${variant.description}</p>
+        ${/* TODO render inputs */}
+      </div>
+    `.trim();
+    rootElement.appendChild(sectionElement);
+  }
+}
 
-  const { housing, hackers } = await loadHousingData();
+export async function main({ formElement, variantsRootElement }) {
+  const selectElement = formElement.elements.type;
+
   const profile = getHackerSlackProfile();
+  const { housing, hackers, types, variants } = await loadHousingData();
+
+  renderHousingTypes(selectElement, { types, formElement });
+  renderHousingVariants(variantsRootElement, { variants, housing });
+
   const hacker = hackers.find(({ sub }) => sub === profile.sub);
 
   {
