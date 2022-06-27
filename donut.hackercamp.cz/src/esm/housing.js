c2a5887cfe4d21cf49c68b94d8a2585a9a545d22
Daniel Hromada <hromada.dan@gmail.com>
2022-06-27 09:15:53 +0200

    #104 Hackers housing data mock
    And bits of wording.

--- donut.hackercamp.cz/src/esm/housing.js
+++ donut.hackercamp.cz/src/esm/housing.js
@@ -1,6 +1,16 @@
 //
-export function main({ formElement, sectionElements }) {
-  formElement.elements.type.addEventListener("change", (event) => {
+
+async function loadHousingData() {
+  const responses = await Promise.all([
+    fetch(`/housing/index.json`),
+    fetch(`/housing/hackers.json`),
+  ]);
+  const [housing, hackers] = await Promise.all(responses.map((r) => r.json()));
+  return { housing, hackers };
+}
+
+function watchHousingTypeSwitch({ selectElement, sectionElements }) {
+  selectElement.addEventListener("change", (event) => {
     const { value } = event.target;
     for (let sectionElm of sectionElements) {
       if (Array.from(sectionElm.classList).includes(`${value}-housing`)) {
@@ -10,10 +20,57 @@ export function main({ formElement, sectionElements }) {
       }
     }
   });
+}
 
-  setTimeout(() => {
-    const selectElm = formElement.elements.type;
-    selectElm.value = "glamping";
-    selectElm.dispatchEvent(new Event("change"));
-  }, 200);
+function getHackerSlackProfile() {
+  const profile = JSON.parse(localStorage.getItem("slack:profile") || "{}");
+  return profile;
+}
+
+function renderHousing() {
+  // TODO
+  `
+    <h3>Stan č. 1</h3>
+    <div class="booking-grid">
+      <div class="booking-grid__cell">
+        <input list="hackers" name="glamping[]" placeholder="-- Volno --" />
+      </div>
+      <div class="booking-grid__cell">
+        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
+      </div>
+      <div class="booking-grid__cell">
+        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
+      </div>
+      <div class="booking-grid__cell">
+        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
+      </div>
+      <div class="booking-grid__cell">
+        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
+      </div>
+      <div class="booking-grid__cell">
+        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
+      </div>
+    </div>
+  `;
+}
+
+export async function main({ formElement, sectionElements }) {
+  const selectElement = formElement.elements.type;
+  watchHousingTypeSwitch({ selectElement, sectionElements });
+
+  const { housing, hackers } = await loadHousingData();
+  const profile = getHackerSlackProfile();
+  const hacker = hackers.find(({ sub }) => sub === profile.sub);
+
+  {
+    if (hacker?.housing?.type) {
+      selectElement.value = hacker.housing.type;
+    }
+    selectElement.dispatchEvent(new Event("change"));
+  }
+
+  // loop over housing to render options
+  for (let { type, sign, capacity, color } of housing) {
+    console.log({ type, sign, capacity, color });
+  }
 }
