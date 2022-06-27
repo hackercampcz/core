9b3a0bb3c720d7b417253dc50bb043fa5ce1e5ac
Daniel Hromada <hromada.dan@gmail.com>
2022-06-27 14:38:02 +0200

    #104 render inputs and hackers

--- donut.hackercamp.cz/src/esm/housing.js
+++ donut.hackercamp.cz/src/esm/housing.js
@@ -13,16 +13,18 @@ async function loadHousingData() {
   return { housing, types, variants, hackers };
 }
 
-function renderHousingTypes(selectElement, { types, formElement }) {
+function renderHousingTypes(
+  selectElement,
+  { types, formElement, hackerHousing }
+) {
   for (const type of types) {
     const option = document.createElement("option");
     option.value = type.name;
+    option.selected = type.name === hackerHousing.type;
     option.textContent = type.title;
     selectElement.appendChild(option);
   }
-  selectElement.addEventListener("change", (event) => {
-    const { value } = event.target;
-
+  selectElement.addEventListener("change", ({ target: { value } }) => {
     for (let section of formElement.querySelectorAll("section")) {
       if (section.classList.contains(`${value}-housing`)) {
         section.setAttribute("aria-hidden", "false");
@@ -38,47 +40,74 @@ function getHackerSlackProfile() {
   return profile;
 }
 
-function renderHousing() {
-  // TODO
-  `
-    <h3>Stan č. 1</h3>
-    <div class="booking-grid">
-      <div class="booking-grid__cell">
-        <input list="hackers" name="glamping[]" placeholder="-- Volno --" />
-      </div>
-      <div class="booking-grid__cell">
-        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
-      </div>
-      <div class="booking-grid__cell">
-        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
-      </div>
-      <div class="booking-grid__cell">
-        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
-      </div>
-      <div class="booking-grid__cell">
-        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
-      </div>
-      <div class="booking-grid__cell">
-        <input list="hackers" name="glamping[]" placeholder="-- Volno --"  />
-      </div>
-    </div>
-  `;
-}
-
 function renderHousingVariants(rootElement, { variants, housing }) {
   for (const variant of variants) {
     const sectionElement = document.createElement("section");
     sectionElement.classList.add(`${variant.type}-housing`);
     sectionElement.setAttribute("aria-hidden", "true");
+    const housingOfVariant = housing.filter(
+      (x) => x.type === variant.type && x.variant === variant.name
+    );
     sectionElement.innerHTML = `
       <h2>${variant.title}</h2>
       <div class="hc-card">
         <p>${variant.description}</p>
-        ${/* TODO render inputs */}
+        ${housingOfVariant
+          .map(
+            ({ room, capacity }) => `
+          <h3>${room}</h3>
+          <div class="booking-grid">
+          ${Array.from({ length: capacity })
+            .map(
+              (_, index) => `
+              <div class="booking-grid__cell">
+                <input
+                  list="hackers"
+                  name="housing['${room}'][${index}]"
+                  placeholder="-- Volno --"
+                />
+              </div>
+            `
+            )
+            .join("")}
+          </div>
+        `
+          )
+          .join("")}
       </div>
     `.trim();
     rootElement.appendChild(sectionElement);
   }
+}
+
+function renderHackers(rootElement, { hackers, hacker }) {
+  const hackersList = document.createElement("datalist");
+  hackersList.id = "hackers";
+
+  for (const { sub, firstName, lastName, company, housing } of hackers) {
+    const isHomeless = !housing;
+    const inlineValue = `${firstName} ${lastName} z ${company}`;
+
+    if (isHomeless) {
+      const option = document.createElement("option");
+      option.value = inlineValue;
+      hackersList.appendChild(option);
+    } else {
+      const inputElement = rootElement.querySelector(`
+        input[name^="housing['${housing}']"]:placeholder-shown,
+        [value="${housing}"]
+      `);
+      inputElement.value = inlineValue;
+
+      if (sub === hacker.sub) {
+        inputElement.classList.add("me");
+      } else {
+        inputElement.disabled = true;
+      }
+    }
+  }
+
+  rootElement.appendChild(hackersList);
 }
 
 export async function main({ formElement, variantsRootElement }) {
@@ -87,20 +116,12 @@ export async function main({ formElement, variantsRootElement }) {
   const profile = getHackerSlackProfile();
   const { housing, hackers, types, variants } = await loadHousingData();
 
-  renderHousingTypes(selectElement, { types, formElement });
-  renderHousingVariants(variantsRootElement, { variants, housing });
-
   const hacker = hackers.find(({ sub }) => sub === profile.sub);
+  const hackerHousing = housing.find(({ room }) => room === hacker.housing);
 
-  {
-    if (hacker?.housing?.type) {
-      selectElement.value = hacker.housing.type;
-    }
-    selectElement.dispatchEvent(new Event("change"));
-  }
+  renderHousingTypes(selectElement, { types, formElement, hackerHousing });
+  renderHousingVariants(variantsRootElement, { variants, housing });
+  selectElement.dispatchEvent(new Event("change"));
 
-  // loop over housing to render options
-  for (let { type, sign, capacity, color } of housing) {
-    console.log({ type, sign, capacity, color });
-  }
+  renderHackers(formElement, { hackers, hacker });
 }
