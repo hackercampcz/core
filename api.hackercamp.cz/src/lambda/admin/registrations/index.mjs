11646a15031baf384c79724ede2e54a104c26676
Aleš Roubíček <ales@roubicek.name>
2022-07-28 09:48:40 +0200

    [api] Optout creation

--- api.hackercamp.cz/src/lambda/admin/registrations/index.mjs
+++ api.hackercamp.cz/src/lambda/admin/registrations/index.mjs
@@ -6,6 +6,13 @@ import * as post from "./post.mjs";
 /** @typedef { import("@pulumi/awsx/apigateway").Request } APIGatewayProxyEvent */
 /** @typedef { import("@pulumi/awsx/apigateway").Response } APIGatewayProxyResult */
 
+async function checkAuthorization(event) {
+  const token = getToken(event.headers);
+  const privateKey = process.env["private_key"];
+  const isAuthorized = await authorize("admin", token, privateKey);
+  if (!isAuthorized) throw Error("Unauthorized");
+}
+
 /**
  * @param {APIGatewayProxyEvent} event
  * @returns {Promise.<APIGatewayProxyResult>}
@@ -13,24 +20,16 @@ import * as post from "./post.mjs";
 export async function handler(event) {
   const withCORS_ = withCORS(
     ["GET", "POST", "OPTIONS"],
-    event.headers?.origin ?? "*",
-    {
-      allowCredentials: true,
-    }
+    event?.headers?.origin ?? "*",
+    { allowCredentials: true }
   );
   try {
-    const isAuthorized = await authorize(
-      "admin",
-      getToken(event.headers),
-      process.env["private_key"]
-    );
-    if (!isAuthorized) {
-      throw Error("Unauthorized");
-    }
     switch (event.httpMethod) {
       case "GET":
+        await checkAuthorization(event);
         return get.handler(event).then((x) => withCORS_(x));
       case "POST":
+        await checkAuthorization(event);
         return post.handler(event).then((x) => withCORS_(x));
       case "OPTIONS":
         return withCORS_({
--- api.hackercamp.cz/src/lambda/admin/registrations/post.mjs
+++ api.hackercamp.cz/src/lambda/admin/registrations/post.mjs
@@ -10,12 +10,44 @@ import { accepted, internalError, readPayload, seeOther } from "../../http.mjs";
 const db = new DynamoDBClient({});
 
 /**
+ * @param {DynamoDBClient} db
+ * @param {{email: string, year: number}} data
+ */
+async function optout(db, { email, year }) {
+  return db.send(
+    new PutItemCommand({
+      TableName: "hc-optouts",
+      Item: marshall(
+        { email, year },
+        {
+          convertEmptyValues: true,
+          removeUndefinedValues: true,
+        }
+      ),
+    })
+  );
+}
+
+/**
+ * @param {DynamoDBClient} db
+ * @param {*} data
+ */
+async function processRequest(db, data) {
+  switch (data.command) {
+    case "optout":
+      await optout(db, data.params);
+      break;
+  }
+}
+
+/**
  * @param {APIGatewayProxyEvent} event
  * @returns {Promise.<APIGatewayProxyResult>}
  */
 export async function handler(event) {
   try {
     const data = readPayload(event);
+    await processRequest(db, data);
     if (event.headers.Accept === "application/json") {
       return accepted();
     }
