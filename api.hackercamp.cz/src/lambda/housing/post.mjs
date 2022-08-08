17ea6525452d380ba18988d5c6d7a6763f01a27b
Aleš Roubíček <ales@roubicek.name>
2022-08-08 12:34:24 +0200

    [api] Housing logging

--- api.hackercamp.cz/src/lambda/housing/post.mjs
+++ api.hackercamp.cz/src/lambda/housing/post.mjs
@@ -1,6 +1,7 @@
 import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { selectKeys } from "@hackercamp/lib/object.mjs";
+import { getToken, validateToken } from "@hackercamp/lib/auth.mjs";
 import { accepted, internalError, readPayload, seeOther } from "../http.mjs";
 
 /** @typedef { import("@aws-sdk/client-dynamodb").DynamoDBClient } DynamoDBClient */
@@ -34,10 +35,14 @@ function saveAttendee(dynamo, data) {
 export async function handler(event) {
   try {
     const data = readPayload(event);
+    const token = getToken(event.headers);
+    const payload = await validateToken(token, process.env.private_key);
+    console.dir(payload, { depth: 10 });
     const year = parseInt(data.year, 10);
-    console.log({ method: "POST", data });
+    console.dir({ method: "POST", data }, { depth: 10 });
     for (const item of data.items) {
       await saveAttendee(dynamo, Object.assign({ year }, item));
+      // TODO: send Slack message
     }
     if (event.headers.Accept === "application/json") {
       return accepted();
