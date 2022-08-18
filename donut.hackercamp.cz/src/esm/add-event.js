0e7eed772ff73d11643d81e16240c80ba9c193bc
Daniel Hromada <hromada.dan@gmail.com>
2022-08-18 08:31:25 +0200

    #178 hard copies of add event form for all types

--- donut.hackercamp.cz/src/esm/add-event.js
+++ donut.hackercamp.cz/src/esm/add-event.js
@@ -23,33 +23,81 @@ export function renderAddEvent() {
             href="#"
             @click=${(event) => {
               event.preventDefault();
-              transact((x) => Object.assign(x, { view: renderTalk }));
+              transact((x) =>
+                Object.assign(x, { view: renderTalk, lineup: "limain" })
+              );
             }}
             >Mainframe</a
           >
         </div>
         <div>
           <p>Data. Dev. Hacks.</p>
-          <a class="hc-link--decorated" href="#">Basecamp</a>
+          <a
+            class="hc-link--decorated"
+            href="#"
+            @click=${(event) => {
+              event.preventDefault();
+              transact((x) =>
+                Object.assign(x, { view: renderTalk, lineup: "libase" })
+              );
+            }}
+            >Basecamp</a
+          >
         </div>
       </div>
       <div class="hc-card hc-card--decorated">
         <h3>Workshop</h3>
-        <a class="hc-link--decorated" href="#">Backend</a>
+        <a
+          class="hc-link--decorated"
+          href="#"
+          @click=${(event) => {
+            event.preventDefault();
+            transact((x) =>
+              Object.assign(x, { view: renderWorkshop, lineup: "liback" })
+            );
+          }}
+          >Backend</a
+        >
       </div>
       <div class="hc-card hc-card--decorated">
         <h3>Sport / Meditace</h3>
-        <a class="hc-link--decorated" href="#">PeopleWare</a>
+        <a
+          class="hc-link--decorated"
+          href="#"
+          @click=${(event) => {
+            event.preventDefault();
+            transact((x) =>
+              Object.assign(x, { view: renderSport, lineup: "lipeep" })
+            );
+          }}
+          >PeopleWare</a
+        >
       </div>
       <div class="hc-card hc-card--decorated">
         <h3>Hudební program</h3>
-        <a class="hc-link--decorated" href="#"
+        <a
+          class="hc-link--decorated"
+          href="#"
+          @click=${(event) => {
+            event.preventDefault();
+            transact((x) => Object.assign(x, { view: renderMusic }));
+          }}
           >WoodStack /<br />Jungle release</a
         >
       </div>
       <div class="hc-card hc-card--decorated">
         <h3>Další / jiný doprovodný program</h3>
-        <a class="hc-link--decorated" href="#">Doprovodný program</a>
+        <a
+          class="hc-link--decorated"
+          href="#"
+          @click=${(event) => {
+            event.preventDefault();
+            transact((x) =>
+              Object.assign(x, { view: renderOther, lineup: "lia10g" })
+            );
+          }}
+          >Doprovodný program</a
+        >
       </div>
     </section>
     <hr />
@@ -57,7 +105,7 @@ export function renderAddEvent() {
   `;
 }
 
-export function renderTalk({ apiUrl, profile, year }) {
+export function renderTalk({ apiUrl, profile, year, lineup = "" }) {
   return html`
     <h2>Talk / Přednáška / Diskuse</h2>
     <p>Byznys. Life. NGOs. BioHacks. Byznys stories</p>
@@ -65,7 +113,7 @@ export function renderTalk({ apiUrl, profile, year }) {
       <input type="hidden" name="type" value="talk" />
       <input type="hidden" name="slackID" value=${profile.sud} />
       <input type="hidden" name="year" value=${year} />
-      <input type="hidden" name="referer" value="/program" />
+      <input type="hidden" name="lineup" value=${lineup} />
       <div class="field">
         <label for="title">Název přednášky, anotace </label>
         <input id="title" name="title" type="text" required />
@@ -87,6 +135,173 @@ export function renderTalk({ apiUrl, profile, year }) {
           <label for="buddy">Parťák (nepovinnej)</label>
           <input id="buddy" name="buddy" type="text" />
         </div>
+      </div>
+      <button type="submit" class="hc-button">Odeslat to</button>
+    </form>
+  `;
+}
+
+export function renderWorkshop({ apiUrl, profile, year, lineup = "" }) {
+  return html`
+    <h2>Workshop</h2>
+    <!-- <p>Byznys. Life. NGOs. BioHacks. Byznys stories</p> -->
+    <form method="post" action="${apiUrl}program">
+      <input type="hidden" name="type" value="talk" />
+      <input type="hidden" name="slackID" value=${profile.sud} />
+      <input type="hidden" name="year" value=${year} />
+      <input type="hidden" name="lineup" value=${lineup} />
+      <div class="field">
+        <label for="title">Název workshopu, anotace </label>
+        <input id="title" name="title" type="text" required />
+      </div>
+      <div class="group">
+        <div class="field">
+          <label for="duration">Délka v minutách</label>
+          <input
+            id="duration"
+            name="duration"
+            type="number"
+            min="15"
+            max="120"
+            value="90"
+            required
+          />
+        </div>
+        <div class="field" style="flex: 2">
+          <label for="buddy">Parťák (nepovinnej)</label>
+          <input id="buddy" name="buddy" type="text" />
+        </div>
+      </div>
+      <button type="submit" class="hc-button">Odeslat to</button>
+    </form>
+  `;
+}
+
+export function renderSport({ apiUrl, profile, year, lineup = "" }) {
+  return html`
+    <h2>Sport / Meditace</h2>
+    <!-- <p>Byznys. Life. NGOs. BioHacks. Byznys stories</p> -->
+    <form method="post" action="${apiUrl}program">
+      <input type="hidden" name="type" value="talk" />
+      <input type="hidden" name="slackID" value=${profile.sud} />
+      <input type="hidden" name="year" value=${year} />
+      <input type="hidden" name="lineup" value=${lineup} />
+      <div class="field">
+        <label for="title"
+          >Název aktivity, anotace (co si pod tím představit)</label
+        >
+        <input id="title" name="title" type="text" required />
+      </div>
+      <div class="group">
+        <div class="field">
+          <label for="duration">Délka v minutách</label>
+          <input
+            id="duration"
+            name="duration"
+            type="number"
+            min="15"
+            max="120"
+            value="60"
+            required
+          />
+        </div>
+        <div class="field" style="flex: 2">
+          <label for="buddy">Parťák (nepovinnej)</label>
+          <input id="buddy" name="buddy" type="text" />
+        </div>
+      </div>
+      <div class="field">
+        <label for="palce"
+          >Místo, kde se sejdete / kde se aktivta bude kontat</label
+        >
+        <input id="palce" name="palce" type="text" required />
+      </div>
+      <button type="submit" class="hc-button">Odeslat to</button>
+    </form>
+  `;
+}
+
+export function renderMusic({ apiUrl, profile, year }) {
+  return html`
+    <h2>Sport / Meditace</h2>
+    <!-- <p>Byznys. Life. NGOs. BioHacks. Byznys stories</p> -->
+    <form method="post" action="${apiUrl}program">
+      <input type="hidden" name="type" value="talk" />
+      <input type="hidden" name="slackID" value=${profile.sud} />
+      <input type="hidden" name="year" value=${year} />
+      <div class="field">
+        <label for="name">DJ name / umělecké jméno</label>
+        <input id="name" name="name" type="text" required />
+      </div>
+      <div class="field field--block">
+        <label for="performance">
+          Popiš svoje vystoupení (žánr, solo / kapela / hudební těleso)
+        </label>
+        <textarea id="performance" name="performance" rows="5"></textarea>
+      </div>
+      <div class="group">
+        <div class="field">
+          <label for="duration">Délka</label>
+          <input
+            id="duration"
+            name="duration"
+            type="number"
+            min="15"
+            max="120"
+            value="60"
+            required
+          />
+        </div>
+        <div class="field" style="flex: 2">
+          <label for="buddy">Parťák (nepovinnej)</label>
+          <input id="buddy" name="buddy" type="text" />
+        </div>
+      </div>
+      <button type="submit" class="hc-button">Odeslat to</button>
+    </form>
+  `;
+}
+export function renderOther({ apiUrl, profile, year }) {
+  return html`
+    <h2>Další / jiný doprovodný program</h2>
+    <!-- <p>Byznys. Life. NGOs. BioHacks. Byznys stories</p> -->
+    <form method="post" action="${apiUrl}program">
+      <input type="hidden" name="type" value="talk" />
+      <input type="hidden" name="slackID" value=${profile.sud} />
+      <input type="hidden" name="year" value=${year} />
+      <div class="field">
+        <label for="title"
+          >Název aktivity, anotace (co si pod tím představit)</label
+        >
+        <input id="title" name="title" type="text" required />
+      </div>
+      <div class="group">
+        <div class="field">
+          <label for="duration">Délka</label>
+          <input
+            id="duration"
+            name="duration"
+            type="number"
+            min="15"
+            max="120"
+            value="60"
+            required
+          />
+        </div>
+        <div class="field" style="flex: 2">
+          <label for="buddy">Parťák (nepovinnej)</label>
+          <input id="buddy" name="buddy" type="text" />
+        </div>
+      </div>
+      <div class="field field--block">
+        <label for="demands"> Máš specifické požadavky, aby to klaplo? </label>
+        <textarea id="demands" name="demands" rows="5"></textarea>
+      </div>
+      <div class="field">
+        <label for="palce"
+          >Místo, kde se sejdete / kde se aktivta bude kontat</label
+        >
+        <input id="palce" name="palce" type="text" required />
       </div>
       <button type="submit" class="hc-button">Odeslat to</button>
     </form>
