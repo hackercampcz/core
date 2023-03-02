43ccdc9b4f40ccfd3c23e8f52ca76f570c189de9
Aleš Roubíček <ales@roubicek.name>
2023-03-02 12:46:54 +0100

    dafak?

--- infrastructure/communication.ts
+++ infrastructure/communication.ts
@@ -1,7 +1,7 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
-import { marked } from "marked";
 
+const { marked } = require("marked");
 const frontmatter = require("front-matter");
 
 export function* readTemplates(relPath) {
