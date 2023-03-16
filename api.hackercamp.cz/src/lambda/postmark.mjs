9be3abb489c2c609d695e9943973ebe3208edffe
Aleš Roubíček <ales@roubicek.name>
2023-03-16 11:27:55 +0100

    [communication] Added new mail template for paid registartions

--- api.hackercamp.cz/src/lambda/postmark.mjs
+++ api.hackercamp.cz/src/lambda/postmark.mjs
@@ -2,23 +2,24 @@ export const Template = {
   HackerRegistration: parseInt(process.env["hc-hacker-registration"], 10),
   NewRegistration: parseInt(process.env["hc-registration-new"], 10),
   RegistrationApproved: parseInt(process.env["hc-registration-approved"], 10),
+  RegistrationPaid: parseInt(process.env["hc-registration-paid"], 10),
   PlusOneInvitation: parseInt(process.env["hc-plus-one-invitation"], 10),
   PlusOneRegistration: parseInt(process.env["hc-plus-one-registration"], 10),
   SlackInvite: parseInt(process.env["hc-slack-invitation"], 10),
-  VolunteerSlackInvite:  parseInt(process.env["hc-volunteer-slack-invitation"], 10),
-  HackerInvitation:  parseInt(process.env["hc-hacker-invitation"], 10),
-  AttendeesInfoMail:  parseInt(process.env["hc-attendee-info"], 10),
+  VolunteerSlackInvite: parseInt(process.env["hc-volunteer-slack-invitation"], 10),
+  HackerInvitation: parseInt(process.env["hc-hacker-invitation"], 10),
+  AttendeesInfoMail: parseInt(process.env["hc-attendee-info"], 10),
   HackerInvitationLate: 28122158,
 };
 
 export async function sendEmailWithTemplate({
-  token,
-  templateId,
-  data,
-  from,
-  to,
-}) {
-  console.log({ event: "Sending mail", to, templateId });
+                                              token,
+                                              templateId,
+                                              data,
+                                              from,
+                                              to,
+                                            }) {
+  console.log({event: "Sending mail", to, templateId});
   const resp = await fetch("https://api.postmarkapp.com/email/withTemplate", {
     method: "POST",
     headers: {
