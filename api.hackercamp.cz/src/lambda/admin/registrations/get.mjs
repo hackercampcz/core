7982271fb86a4a8bc5d8c2f6dcf552f0a17725a2
Aleš Roubíček <ales@roubicek.name>
2023-03-27 14:30:26 +0200

    [api] Load Confirmed from Algolia

--- api.hackercamp.cz/src/lambda/admin/registrations/get.mjs
+++ api.hackercamp.cz/src/lambda/admin/registrations/get.mjs
@@ -1,4 +1,8 @@
-import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import {
+  BatchGetItemCommand,
+  DynamoDBClient,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import createSearchClient from "algoliasearch";
 import { response, internalError, notFound } from "../../http.mjs";
@@ -37,34 +41,61 @@ async function getOptOuts(year) {
 
 /**
  *
- * @param {SearchIndex} index
- * @param {number} year
- * @returns {Promise<Record<string, any>[]>}
+ * @param {DynamoDBClient} db
+ * @param hits
+ * @returns {Promise<*|*[]>}
  */
-async function getConfirmedRegistrations(index, year) {
-  console.log("Loading confirmed hackers data", { year });
-  const { hits, ...searchResult } = await index.search("", {
-    tagFilters: [year.toString(), "confirmed"],
-  });
-  console.log({ hits, searchResult });
-  const res = await db.send(
-    new ScanCommand({
-      TableName: process.env.db_table_registrations,
-      Select: "ALL_ATTRIBUTES",
-      FilterExpression:
-        "#yr = :yr AND attribute_not_exists(invoiced) AND (firstTime = :false OR (attribute_exists(referral) AND attribute_type(referral, :string)))",
-      ExpressionAttributeNames: { "#yr": "year" },
-      ExpressionAttributeValues: marshall(
-        {
-          ":false": false,
-          ":yr": year,
-          ":string": "S",
-        },
-        { removeUndefinedValues: true }
-      ),
+async function getItemsFromDB(db, hits) {
+  if (hits.length === 0) return [];
+  const tableName = process.env.db_table_registrations;
+  const keys = hits.map(({ year, email }) => ({
+    year: { N: year },
+    email: { S: email },
+  }));
+  const result = await db.send(
+    new BatchGetItemCommand({
+      RequestItems: { [tableName]: { Keys: keys } },
     })
   );
-  return res.Items.map((x) => unmarshall(x));
+  return result.Responses[tableName]
+    .map((x) => unmarshall(x))
+    .sort((a, b) => -1 * a.timestamp?.localeCompare(b.timestamp));
+}
+
+/**
+ *
+ * @param {SearchIndex} index
+ * @param {number} year
+ * @param {number} page
+ * @returns {Promise<Record<string, any>[]>}
+ */
+async function getConfirmedRegistrations(index, year, page) {
+  console.log("Loading confirmed hackers data", { year, page });
+  const { hits, ...searchResult } = await index.search("", {
+    attributesToRetrieve: ["year", "email"],
+    tagFilters: [year.toString(), "confirmed"],
+    // TODO: make page size reasonably small
+    hitsPerPage: 300,
+    page
+  });
+  // searchResult: {
+  //     nbHits: 40,
+  //     page: 0,
+  //     nbPages: 2,
+  //     hitsPerPage: 20,
+  //     exhaustiveNbHits: true,
+  //     exhaustiveTypo: true,
+  //     exhaustive: { nbHits: true, typo: true },
+  //     query: '',
+  //     params: 'tagFilters=%5B%222023%22%2C%22confirmed%22%5D',
+  //     renderingContent: {},
+  //     processingTimeMS: 1,
+  //     processingTimingsMS: { getIdx: [Object], request: [Object], total: 1 },
+  //     serverTimeMS: 2
+  //   }
+  const res = await getItemsFromDB(db, hits);
+  // TODO: pagination
+  return res;
 }
 
 async function getHackersRegistrations(year) {
@@ -137,11 +168,11 @@ async function getPaidRegistrations(year) {
   return res.Items.map((x) => unmarshall(x));
 }
 
-function getData(type, year) {
+function getData(type, year, page) {
   const index = openAlgoliaIndex();
   switch (type) {
     case "confirmed":
-      return getConfirmedRegistrations(index, year);
+      return getConfirmedRegistrations(index, year, page);
     case "hackers":
       return getHackersRegistrations(year);
     case "invoiced":
@@ -163,14 +194,14 @@ function getData(type, year) {
  */
 export async function handler(event) {
   console.log("QS", event.queryStringParameters);
-  const { type, year } = Object.assign(
-    { year: "2022" },
+  const { type, year, page } = Object.assign(
+    { year: "2022", page: "0" },
     event.queryStringParameters
   );
   try {
     const [optouts, data] = await Promise.all([
       getOptOuts(parseInt(year)),
-      getData(type, parseInt(year)),
+      getData(type, parseInt(year), parseInt(page)),
     ]);
     if (type === "optouts") return response(Array.from(optouts));
     if (!data) return notFound();
