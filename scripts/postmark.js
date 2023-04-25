27983ea3b7814b1a86710d7910b992d76ef06a84
Aleš Roubíček <ales@roubicek.name>
2023-04-25 19:49:18 +0200

    [scripts] Update Postmark templates

--- scripts/postmark.js
+++ scripts/postmark.js
@@ -3,16 +3,12 @@ export const Template = {
   PlusOneRegistration: 30944876,
   HackerRegistration: 30944917,
   HackerInvitation: 30945225,
-  HackerInvitationLate: 28122158,
-  HackersReminder: 28432253,
   PlusOneInvitation: 30945167,
   VolunteerSlackInvite: 30945168,
   SlackInvite: 30945181,
   AttendeesInfoMail: 30945078,
-  LastYearHackersPush: 28896807,
-  ThisYearsHackersPush: 28896811,
-  Feedback: 29143589,
   VolunteerInvitation: 31455038,
+  HackerPush: 31533299,
 };
 
 export async function sendEmailWithTemplate({
