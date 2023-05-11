3869d2748a79695eb9c782380b161d926916696e
Aleš Roubíček <ales@roubicek.name>
2023-05-11 14:36:29 +0200

    Fix start date

--- donut.hackercamp.cz/src/data/global.mjs
+++ donut.hackercamp.cz/src/data/global.mjs
@@ -2,7 +2,7 @@ export default {
   year: 2023,
   event: {
     name: "Hacker Camp",
-    startDate: new Date("2023-05-12T17:00:00+02:00"),
+    startDate: new Date("2023-08-31T17:00:00+02:00"),
     endDate: new Date("2023-09-03T12:00:00+02:00"),
     get willStartSoon() {
       const today = new Date();
