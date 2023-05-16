25ea450f2c174200d910850ae31c6ed9dfaf2111
Aleš Roubíček <ales@roubicek.name>
2023-05-16 19:38:18 +0200

    [api] tidy up all headers

--- api.hackercamp.cz/src/lambda/admin/registrations/get.mjs
+++ api.hackercamp.cz/src/lambda/admin/registrations/get.mjs
@@ -108,18 +108,21 @@ async function getRegistrations(query, tag, year, page, pageSize) {
   };
 }
 
+function getAllHeaders(data) {
+  const headers = new Set();
+  for (const item of data.items) {
+    for (const key of Object.keys(item)) {
+      headers.add(key);
+    }
+  }
+  return Array.from(headers);
+}
+
 async function formatResponse(data, { year, type, format }) {
   if (format === "csv" || format === "text/csv") {
     console.log({ event: "Formatting CSV" });
-    const headers = new Set();
-    for (const item of data.items) {
-      for (const key of Object.keys(item)) {
-        headers.add(key);
-      }
-    }
-    const text = await csv.writeToString(data.items, {
-      headers: Array.from(headers),
-    });
+    const headers = getAllHeaders(data);
+    const text = await csv.writeToString(data.items, { headers });
     const fileName = `hc-${year}-registrations-${type}.csv`;
     return response(text, {
       "Content-Type": "text/csv",
