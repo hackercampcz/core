ae62da0d8c4e1573a7aeed1f15a911c81250e3b1
Daniel Hromada <hromada.dan@gmail.com>
2023-08-30 12:52:08 +0200

    [donut] Enable unlock only not backstage deds

--- donut.hackercamp.cz/src/esm/housing.js
+++ donut.hackercamp.cz/src/esm/housing.js
@@ -289,6 +289,7 @@ function renderBackstage(rootElement, { backstage }) {
     )) {
       inputElement.value = label;
       inputElement.disabled = true;
+      inputElement.parentElement.querySelector("button").remove();
     }
   }
 }
