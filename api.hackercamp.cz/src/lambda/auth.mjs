97c80dfd7e6523cc5ec9994068f5701eba2ddbf7
Aleš Roubíček <ales@roubicek.name>
2024-03-28 17:55:13 +0100

    logging

--- api.hackercamp.cz/src/lambda/auth.mjs
+++ api.hackercamp.cz/src/lambda/auth.mjs
@@ -2,6 +2,8 @@ import { getToken, validateToken } from "@hackercamp/lib/auth.mjs";
 
 export async function checkAuthorization(event) {
   const token = getToken(event.headers);
-  const isAuthorized = await validateToken(token, process.env["private_key"]);
+  const pkey = process.env["private_key"];
+  console.log({ event: "check-authorization", token, pkey });
+  const isAuthorized = await validateToken(token, pkey);
   if (!isAuthorized) throw Error("Unauthorized");
 }
