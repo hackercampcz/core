95882ac776748729daeb46ed0c487feb8d8b8f05
Aleš Roubíček <ales@roubicek.name>
2024-04-14 21:28:39 +0200

    Update postmark template IDs  and add function to send batches

--- scripts/lib/postmark.js
+++ scripts/lib/postmark.js
@@ -1,16 +1,15 @@
 export const Template = {
-  // TODO : update
-  NewRegistration: 30944901,
-  PlusOneRegistration: 30944876,
-  HackerRegistration: 30944917,
-  HackerInvitation: 30945225,
-  PlusOneInvitation: 30945167,
-  VolunteerSlackInvite: 30945168,
-  SlackInvite: 30945181,
-  AttendeesInfoMail: 30945078,
-  VolunteerInvitation: 31455038,
-  HackerPush: 31533299,
-  Feedback: 33045403,
+  NewRegistration: 34068966,
+  PlusOneRegistration: 34068946,
+  HackerRegistration: 34068967,
+  HackerInvitation: 34068964,
+  PlusOneInvitation: 34068947,
+  VolunteerSlackInvite: 34068978,
+  SlackInvite: 34068981,
+  AttendeesInfoMail: 34068963,
+  VolunteerInvitation: 34068992,
+  HackerPush: 34068980,
+  Feedback: 34068945,
 };
 
 export async function sendEmailWithTemplate({
@@ -41,6 +40,65 @@ export async function sendEmailWithTemplate({
     ),
   );
   const resp = await fetch("https://api.postmarkapp.com/email/withTemplate", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Postmark-Server-Token": token,
+    },
+    body,
+  });
+  if (!resp.ok) {
+    console.error(await resp.json());
+    throw new Error("Mail not send");
+  }
+  return resp.json();
+}
+
+/**
+ * @param token
+ * @param {string[]} emails Max 500 e-mails in one batch
+ * @param templateId
+ * @param templateAlias
+ * @param data
+ * @param from
+ * @param replyTo
+ * @param attachments
+ * @param messageStream
+ * @param tag
+ * @returns {Promise<unknown>}
+ */
+export async function sendEmailsWithTemplate({
+  token,
+  emails,
+  templateId,
+  templateAlias,
+  data,
+  from,
+  replyTo,
+  attachments,
+  messageStream,
+  tag,
+}) {
+  if (emails.length > 500) throw new Error("Maximum number of emails exceeded");
+  const body = JSON.stringify({
+    Messages: emails.map(to =>
+      Object.fromEntries(
+        Object.entries({
+          From: from,
+          To: to,
+          TemplateId: templateId,
+          TemplateAlias: templateAlias,
+          TemplateModel: data,
+          Tag: tag,
+          MessageStream: messageStream,
+          ReplyTo: replyTo,
+          Attachments: attachments,
+        }).filter(([_, v]) => Boolean(v)),
+      )
+    ),
+  });
+  const resp = await fetch("https://api.postmarkapp.com/email/batchWithTemplates", {
     method: "POST",
     headers: {
       Accept: "application/json",
