6f5d2f630dc642c4f94cec0f4157a769d2a355ab
Aleš Roubíček <ales@roubicek.name>
2024-07-19 09:45:42 +0200

    Move redirects to Pages Redirects file

--- www.hackercamp.cz/functions/r/dobrovolnik.js
+++
@@ -1,9 +0,0 @@
-/**
- * Redirects a short registration URL format to full on
- * @param {EventContext<Env>} context
- * @returns {Promise<Response>}
- */
-export async function onRequestGet({}) {
-  const query = new URLSearchParams({ volunteer: 1 });
-  return Response.redirect(`https://www.hackercamp.cz/registrace/?${query}`);
-}
