8cb8f94613f96534dbf6b10fce1266724b1a71b3
Aleš Roubíček <ales@roubicek.name>
2024-08-01 16:02:59 +0200

    final touches

--- api.hackercamp.cz/src/lambda/program/post.mjs
+++ api.hackercamp.cz/src/lambda/program/post.mjs
@@ -1,6 +1,6 @@
 import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
-import { getHeader, readPayload, seeOther } from "../http.mjs";
+import { accepted, readPayload } from "../http.mjs";
 
 /** @typedef { import("@aws-sdk/client-dynamodb").DynamoDBClient } DynamoDBClient */
 /** @typedef { import("@pulumi/awsx/classic/apigateway").Request } APIGatewayProxyEvent */
@@ -38,5 +38,5 @@ export async function handler(event) {
       ),
     }),
   );
-  return seeOther(getHeader(event.headers, "Referer"));
+  return accepted();
 }
