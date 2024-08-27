e62fe3ee1c1b8862566e83d229a60617b0aff620
Aleš Roubíček <ales@roubicek.name>
2024-08-27 18:12:22 +0200

    Allow calendar for unathorized users

--- donut.hackercamp.cz/functions/_middleware.js
+++ donut.hackercamp.cz/functions/_middleware.js
@@ -1,18 +1,14 @@
 import { getToken, validateToken } from "@hackercamp/lib/auth.mjs";
 
-const authorizedOnly = [
-  "/hackers",
-  "/registrace",
-  "/ubytovani",
-  "/program",
-  "/admin",
-];
+const openRoutes = new Set(["/program/kalendar"]);
+const authorizedOnly = ["/hackers", "/registrace", "/ubytovani", "/program", "/admin"];
 
 /**
  * @param {EventContext<Env>} context
  */
 export async function onRequest({ request, next, env }) {
   const url = new URL(request.url);
+  if (openRoutes.has(url.pathname)) return next();
   if (!authorizedOnly.some((x) => url.pathname.startsWith(x))) return next();
 
   const token = getToken(request.headers);
@@ -21,9 +17,6 @@ export async function onRequest({ request, next, env }) {
 
   if (isValidToken) return next();
 
-  const query = new URLSearchParams({
-    state: "not-authenticated",
-    returnUrl: request.url,
-  });
+  const query = new URLSearchParams({ state: "not-authenticated", returnUrl: request.url });
   return Response.redirect(`https://${env.HC_DONUT_HOSTNAME}/?${query}`, 307);
 }
