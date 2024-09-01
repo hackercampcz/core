d4541a6582819505b6756d63b4c3efbe0236c785
Aleš Roubíček <ales@roubicek.name>
2024-09-01 19:31:48 +0200

    Upload AVIF and WebP images to Cloudinary

--- www.hackercamp.cz/config/task-config.mjs
+++ www.hackercamp.cz/config/task-config.mjs
@@ -4,101 +4,64 @@ import pathConfig from "./path-config.json" with { type: "json" };
 
 export default {
   images: false,
-  cloudinary: true,
+  cloudinary: { cloudinary: { extensions: ["jpg", "jpeg", "png", "gif", "avif", "webp", "svg"] } },
   fonts: true,
   svgSprite: true,
   javascripts: false,
   stylesheets: true,
   workboxBuild: false,
 
-  static: {
-    srcConfig: {
-      encoding: false,
-    },
-  },
+  static: { srcConfig: { encoding: false } },
 
-  generate: {
-    redirects: [
-      {
-        collection: "redirects",
-        host: "https://donut.hackercamp.cz",
-        route: (x) => x[0],
-      },
-    ],
-  },
+  generate: { redirects: [{ collection: "redirects", host: "https://donut.hackercamp.cz", route: (x) => x[0] }] },
 
   html: {
     dataFile: "global.mjs",
     collections: ["build", "images"],
     nunjucksRender: {
-      globals: {
-        currentYear: new Date().getFullYear(),
-      },
+      globals: { currentYear: new Date().getFullYear() },
       filters: {
         isoDate(x) {
           return new Date(x).toISOString();
         },
         longDate(x) {
-          return new Intl.DateTimeFormat("cs-CZ", {
-            day: "numeric",
-            month: "long",
-          }).format(new Date(x));
+          return new Intl.DateTimeFormat("cs-CZ", { day: "numeric", month: "long" }).format(new Date(x));
         },
         shortDate(x) {
-          return new Intl.DateTimeFormat("cs-CZ", {
-            day: "numeric",
-            month: "numeric",
-          }).format(new Date(x));
+          return new Intl.DateTimeFormat("cs-CZ", { day: "numeric", month: "numeric" }).format(new Date(x));
         },
         price(x, currency) {
           return new Intl.NumberFormat("cs-CZ", {
             style: currency ? "currency" : undefined,
             currency,
-            maximumFractionDigits: 0,
+            maximumFractionDigits: 0
           }).format(x).replace(/\u00A0/, "\u202F");
         },
         calendarURL(event) {
-          const format = ({ y, m, d }) =>
-            `${y}${m.toString().padStart(2, "0")}${
-              d
-                .toString()
-                .padStart(2, "0")
-            }`;
-          const d = (d) => ({
-            y: d.getFullYear(),
-            m: d.getMonth() + 1,
-            d: d.getDate(),
-          });
+          const format = ({ y, m, d }) => `${y}${m.toString().padStart(2, "0")}${d.toString().padStart(2, "0")}`;
+          const d = (d) => ({ y: d.getFullYear(), m: d.getMonth() + 1, d: d.getDate() });
           const incDay = ({ y, m, d }) => ({ y, m, d: d + 1 });
           return new URL(
             `?${new URLSearchParams({
               action: "TEMPLATE",
-              dates: `${format(d(event.startDate))}/${
-                format(
-                  incDay(d(event.endDate)),
-                )
-              }`,
+              dates: `${format(d(event.startDate))}/${format(incDay(d(event.endDate)))}`,
               text: event.name,
               location: event.location,
               details: event.details,
-              sf: true,
+              sf: true
             })}`,
-            "https://calendar.google.com/calendar/event",
+            "https://calendar.google.com/calendar/event"
           ).toString();
-        },
-      },
-    },
+        }
+      }
+    }
   },
 
   vite: {
     server: { port: 3001 },
     browser: "google chrome canary",
-    browserArgs: "--ignore-certificate-errors --allow-insecure-localhost",
+    browserArgs: "--ignore-certificate-errors --allow-insecure-localhost"
   },
 
-  production: {
-    rev: {
-      exclude: ["favicon.ico", "robots.txt", "humans.txt", "_redirects", "_headers"],
-    },
-  },
+  production: { rev: { exclude: ["favicon.ico", "robots.txt", "humans.txt", "_redirects", "_headers"] } }
 };
