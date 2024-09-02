da25ffd9a29b51c1524bc792b7474ce13b894190
Aleš Roubíček <ales@roubicek.name>
2024-09-02 11:37:07 +0200

    Update will start soon condition

--- www.hackercamp.cz/src/data/global.mjs
+++ www.hackercamp.cz/src/data/global.mjs
@@ -6,18 +6,18 @@ export default {
     endDate: new Date("2024-09-01T12:00:00+02:00"),
     get willStartSoon() {
       const today = new Date();
-      const threeDaysBefore = new Date(this.startDate);
+      const threeDaysBefore = this.startDate;
       threeDaysBefore.setDate(threeDaysBefore.getDate() - 3);
       threeDaysBefore.setHours(0, 0, 0, 0);
-      return today >= threeDaysBefore;
+      return today >= threeDaysBefore && today <= this.endDate;
     },
     location: "Sobeňák, Starý Rožmitál 148, Rožmitál pod Třemšínem",
-    details: "Zažijte netradiční třídenní festival pro lidi z české tech scény.",
+    details: "Zažijte netradiční třídenní festival pro lidi z české tech scény."
   },
   ticket: {
     nonprofit: { name: "Táborník z neziskovky", price: 3000 },
     hacker: { name: "Hacker", price: 7000 },
     "hacker-plus": { name: "Hacker filantrop", price: 12000 },
-    "hacker-patron": { name: "Patron Campu", price: 12000 },
-  },
+    "hacker-patron": { name: "Patron Campu", price: 12000 }
+  }
 };
