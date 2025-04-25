1e56f83770c792576c8f0bb344ebb7365f35a268
Aleš Roubíček <ales@roubicek.name>
2025-04-25 13:59:28 +0200

    Fix query param get

--- api.hackercamp.cz/functions/v2/fakturoid/search.js
+++ api.hackercamp.cz/functions/v2/fakturoid/search.js
@@ -5,7 +5,7 @@ import { getAuthHeader, searchSubject } from "@hackercamp/lib/fakturoid.js";
  * @returns {Promise<Response>}
  */
 export async function onRequestGet({ request, env }) {
-  const query = new URLSearchParams(request.url).get("q");
+  const query = new URL(request.url).searchParams.get("q");
   const authHeader = await getAuthHeader(env.FAKTUROID_CLIENT_ID, env.FAKTUROID_CLIENT_SECRET);
   const result = await searchSubject(authHeader, query);
   return Response.json(result, {
