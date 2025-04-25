68d10e3732d0b231984f21c29e5a94587f1b83f9
Aleš Roubíček <ales@roubicek.name>
2025-04-25 13:36:11 +0200

    Allow Credentials

--- api.hackercamp.cz/functions/v2/fakturoid/search.js
+++ api.hackercamp.cz/functions/v2/fakturoid/search.js
@@ -8,5 +8,9 @@ export async function onRequestGet({ request, env }) {
   const query = new URLSearchParams(request.url).get("q");
   const authHeader = await getAuthHeader(env.FAKTUROID_CLIENT_ID, env.FAKTUROID_CLIENT_SECRET);
   const result = await searchSubject(authHeader, query);
-  return Response.json(result);
+  return Response.json(result, {
+    headers: {
+      "Access-Control-Allow-Credentials": "true"
+    }
+  });
 }
