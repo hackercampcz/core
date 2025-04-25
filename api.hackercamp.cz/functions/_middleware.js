96bd6263b00be67ddca4db4af742efbf9ffbcbf3
Aleš Roubíček <ales@roubicek.name>
2025-04-25 13:18:20 +0200

    CORS middleware

--- api.hackercamp.cz/functions/_middleware.js
+++ api.hackercamp.cz/functions/_middleware.js
@@ -8,4 +8,24 @@ export async function rollbar(context) {
   return rollbarPlugin({ token: context.env.ROLLBAR_TOKEN })(context);
 }
 
-export const onRequest = [rollbar];
+export async function cors(context) {
+  const response = await context.next();
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Max-Age", "86400");
+  return response;
+
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+}
+
+export const onRequest = [rollbar, cors];
