1e8e9968c5195fd177f0c5c1e52f968802575082
Aleš Roubíček <ales@roubicek.name>
2025-04-27 07:56:47 +0200

    try to fix CORS for v1 API

--- api.hackercamp.cz/functions/_middleware.js
+++ api.hackercamp.cz/functions/_middleware.js
@@ -8,24 +8,4 @@ export async function rollbar(context) {
   return rollbarPlugin({ token: context.env.ROLLBAR_TOKEN })(context);
 }
 
-export async function cors(context) {
-  const response = await context.next();
-  response.headers.set("Access-Control-Allow-Origin", context.request.headers.get("origin") ?? "*");
-  response.headers.set("Access-Control-Max-Age", "86400");
-  return response;
-
-}
-
-export async function onRequestOptions() {
-  return new Response(null, {
-    status: 204,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Max-Age": "86400",
-    },
-  });
-}
-
-export const onRequest = [rollbar, cors];
+export const onRequest = [rollbar];
