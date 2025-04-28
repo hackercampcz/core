5f246eff85431afbe214006a2145257273164a6d
Aleš Roubíček <ales@roubicek.name>
2025-04-28 12:56:16 +0200

    Invoicing UI WIP

---
+++ donut.hackercamp.cz/src/esm/invoice.js
@@ -0,0 +1,121 @@
+import { getTicketPrice, ticketName } from "./admin/common.js";
+import { setReturnUrl, signOut } from "./lib/profile.js";
+import { withAuthHandler } from "./lib/remoting.js";
+import * as rollbar from "./lib/rollbar.js";
+
+async function getRegistration(year, email) {
+  const params = new URLSearchParams({ year, email, slackID: "Z" });
+  const resp = await withAuthHandler(fetch(`https://api.hackercamp.cz/v1/registration?${params}`), {
+    onUnauthenticated() {
+      setReturnUrl(location.href);
+      return new Promise((resolve, reject) => {
+        signOut((path) => new URL(path, apiHost).href);
+        reject({ unauthenticated: true });
+      });
+    },
+    onUnauthorized() {
+      return Promise.reject({ unauthorized: true });
+    }
+  });
+  return resp.json();
+}
+
+async function getSubject(q) {
+  const params = new URLSearchParams({ q });
+  const resp = await withAuthHandler(fetch(`https://api.hackercamp.cz/v2/fakturoid/search?${params}`, {
+    credentials: "include",
+    mode: "cors"
+  }), {
+    onUnauthenticated() {
+      setReturnUrl(location.href);
+      return new Promise((resolve, reject) => {
+        signOut((path) => new URL(path, apiHost).href);
+        reject({ unauthenticated: true });
+      });
+    },
+    onUnauthorized() {
+      return Promise.reject({ unauthorized: true });
+    }
+  });
+  return resp.json();
+}
+
+export async function main({ env, searchParams }) {
+  rollbar.init(env);
+
+  if (searchParams.has("modal")) {
+    document.body.classList.add("modal-view");
+  }
+
+  const year = parseInt(searchParams.get("year") ?? env.year);
+  const email = searchParams.get("email");
+
+  if (!email) console.error("No email provided");
+
+  const invoiceForm = document.forms.invoice;
+
+  invoiceForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const resp = await fetch(e.target.action, {
+      method: "POST",
+      body: new URLSearchParams(formData),
+      credentials: "include",
+      mode: "cors"
+    });
+    const data = await resp.json();
+
+    await fetch("https://api.hackercamp.cz/v1/admin/registrations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        command: "invoiced",
+        params: {
+          registrations: [{ year, email }],
+          invoiceId: data.id,
+        }
+      }),
+      credentials: "include",
+      mode: "cors"
+    })
+  });
+
+  invoiceForm.year.value = year;
+  invoiceForm.email.value = email;
+
+  const reg = await getRegistration(year, email);
+
+  document.getElementById("name").textContent = reg.invName;
+  document.getElementById("address").textContent = reg.invAddress;
+  document.getElementById("email").textContent = reg.invEmail ?? reg["invoice-contact"];
+  document.getElementById("regNo").textContent = reg.invRegNo ? `IČO: ${reg.invRegNo}` : "";
+  document.getElementById("vatId").textContent = reg.invVatNo ? `DIČ: ${reg.invVatNo}` : "";
+
+  invoiceForm.note.value = `Hacker Camp ${year}`;
+  invoiceForm.count.value = 1;
+  invoiceForm.text.value = reg.invText ?? ticketName.get(reg.ticketType);
+  invoiceForm.price.value = getTicketPrice(reg);
+
+  const sub = await Promise.all([
+    reg.invRegNo ? getSubject(reg.invRegNo) : null,
+    getSubject(reg.invEmail ?? reg["invoice-contact"]),
+    getSubject(reg.invName),
+  ].filter(Boolean));
+  const subsById = new Map(sub.flat().map(x => [x.id, x]));
+
+  const subjectSet = document.getElementById("subject");
+  const subjectTemplate = subjectSet.querySelector("template").content;
+  if (subsById.size > 0) {
+    const items = document.createDocumentFragment();
+    for (const [id, subject] of subsById) {
+      const itemEl = subjectTemplate.cloneNode(true);
+      itemEl.querySelector("label").insertAdjacentText("beforeend", `${subject.name} ${subject.registration_no ? `(IČO: ${subject.registration_no})` : ""}`);
+      itemEl.querySelector("input").value = id;
+      items.appendChild(itemEl);
+    }
+    subjectSet.querySelector("div").replaceChildren(items);
+    subjectSet.querySelector(`input[value='${Array.from(subsById.keys())[0]}']`).checked = true;
+  }
+}
