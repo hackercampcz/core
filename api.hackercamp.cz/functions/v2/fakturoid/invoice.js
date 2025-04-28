c31301eac8a7487ec627d6b8d9bec128e320961e
Aleš Roubíček <ales@roubicek.name>
2025-04-28 09:00:49 +0200

    Create Invoice API

---
+++ api.hackercamp.cz/functions/v2/fakturoid/invoice.js
@@ -0,0 +1,20 @@
+import { getAuthHeader, createInvoice } from "@hackercamp/lib/fakturoid.js";
+
+/**
+ * @param {EventContext<Env>} context
+ * @returns {Promise<Response>}
+ */
+export async function onRequestPost({ request, env }) {
+  const formData = await request.formData();
+  const authHeader = await getAuthHeader(env.FAKTUROID_CLIENT_ID, env.FAKTUROID_CLIENT_SECRET);
+  const invoice = await createInvoice(authHeader, {
+    subjectId: formData.get("subjectId"),
+    text: formData.get("text"),
+    count: parseInt(formData.get("count")),
+    price: parseInt(formData.get("price"))
+  });
+  return Response.json({
+    id: invoice.id,
+    url: invoice.public_html_url
+  });
+}
