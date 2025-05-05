6be6404919ba232507599bc2649e58b7cae43c04
Aleš Roubíček <ales@roubicek.name>
2025-05-05 16:51:53 +0200

    CORS fighting

--- api.hackercamp.cz/functions/v2/_middleware.js
+++ api.hackercamp.cz/functions/v2/_middleware.js
@@ -6,6 +6,7 @@ export async function onRequestOptions() {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Credentials": "true",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       "Access-Control-Max-Age": "86400",
     },
