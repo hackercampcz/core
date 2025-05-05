8db0f3b20470196dab1489c9c6c9a9a1aeafb5d3
Aleš Roubíček <ales@roubicek.name>
2025-05-05 16:57:45 +0200

    Why?

--- api.hackercamp.cz/functions/v2/fakturoid/_middleware.js
+++ api.hackercamp.cz/functions/v2/fakturoid/_middleware.js
@@ -1,4 +1,3 @@
 import { roleAuthorization } from "../../lib/auth.js";
-import { allowCredentials } from "../../lib/middleware.js";
 
-export const onRequest = [roleAuthorization("admin"), allowCredentials];
+export const onRequest = [roleAuthorization("admin")];
