ba20a36c470b53913d94d54c669299fa99ef175d
Aleš Roubíček <ales@roubicek.name>
2025-06-02 21:22:55 +0200

    Removed unused imports

--- donut.hackercamp.cz/src/esm/admin.js
+++ donut.hackercamp.cz/src/esm/admin.js
@@ -1,7 +1,5 @@
 import "@material/mwc-drawer/mwc-drawer.js";
-import "@material/web/button/text-button.js";
 import "@material/web/textfield/outlined-text-field.js";
-import "@material/web/textfield/filled-text-field.js";
 import { defAtom, updateAsTransaction } from "@thi.ng/atom";
 import { html, render } from "lit-html";
 import { Action, dispatchAction, Endpoint, executeCommand, getDialog, View } from "./admin/common.js";
