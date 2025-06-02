ced4ad845d13a56a6feb08a8726ab94fa9f43e6e
Aleš Roubíček <ales@roubicek.name>
2025-06-02 13:55:16 +0200

    Reworked mail and phone buttons
    #1722

--- donut.hackercamp.cz/src/esm/components/mail-button.js
+++ donut.hackercamp.cz/src/esm/components/mail-button.js
@@ -1,18 +1,29 @@
-import "@material/web/iconbutton/icon-button.js";
-import "@material/web/icon/icon.js";
-import { html, LitElement } from "lit";
+import { html, render } from "lit-html";
 
-export class MailButton extends LitElement {
-  static properties = { email: {} };
+export class MailButton extends HTMLElement {
+
+  get email() {
+    return this.getAttribute("email");
+  }
+
+  connectedCallback() {
+    render(this.render(), this);
+  }
 
   render() {
     if (!this.email) return null;
-    return html`<md-icon-button
-      href="mailto:${this.email}"
-      title="Napsat ${this.email}"
-    >
-      <md-icon>mail</md-icon>
-    </md-icon-button>`;
+    return html`
+      <button class="icon-button" href="mailto:${this.email}" title="Napsat ${this.email}"
+              aria-label="E-mail">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             class="feather feather-mail">
+          <title>E-mail</title>
+          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+          <polyline points="22,6 12,13 2,6"></polyline>
+        </svg>
+      </button>
+    `;
   }
 }
 
--- donut.hackercamp.cz/src/esm/components/phone-button.js
+++ donut.hackercamp.cz/src/esm/components/phone-button.js
@@ -1,26 +1,37 @@
-import "@material/web/iconbutton/icon-button.js";
-import "@material/web/icon/icon.js";
-import { html, LitElement } from "lit";
+import { html, render } from "lit";
 
 function getInternationalPhoneFormat(phone) {
-  let internationalNr = phone.replace(/\s+/g, "");
-  if (internationalNr.startsWith("+")) {
-    return internationalNr;
+  let number = phone.replace(/\s+/g, "");
+  if (number.startsWith("+")) {
+    return number;
   }
-  return `+420${internationalNr}`;
+  return `+420${number}`;
 }
 
-export class PhoneButton extends LitElement {
-  static properties = { phone: {} };
+export class PhoneButton extends HTMLElement {
+
+  get phone() {
+    return this.getAttribute("phone");
+  }
+
+  connectedCallback() {
+    render(this.render(), this);
+  }
 
   render() {
     if (!this.phone) return null;
-    return html`<md-icon-button
-      href="tel:${getInternationalPhoneFormat(this.phone)}"
-      title="Zavolat ${this.phone}"
-    >
-      <md-icon>call</md-icon>
-    </md-icon-button>`;
+    return html`
+      <button class="icon-button" href="tel:${getInternationalPhoneFormat(this.phone)}"
+              title="Zavolat ${this.phone}" aria-label="Telefon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             class="feather feather-phone">
+          <title>Telefon</title>
+          <path
+            d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+        </svg>
+      </button>
+    `;
   }
 }
 
