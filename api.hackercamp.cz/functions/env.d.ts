0a5d224ef8fbec4dfaac0412a59fcea0bdc72d96
Aleš Roubíček <ales@roubicek.name>
2025-06-26 16:23:16 +0200

    Added AWS envs to type def

--- api.hackercamp.cz/functions/env.d.ts
+++ api.hackercamp.cz/functions/env.d.ts
@@ -1,6 +1,10 @@
 export interface Env {
   API_HOST: string;
 
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_REGION: string;
+
   FAKTUROID_CLIENT_ID: string;
   FAKTUROID_CLIENT_SECRET: string;
 
