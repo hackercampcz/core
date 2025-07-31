398ecc09b321058184158a7f98ddd4b221183cf9
Aleš Roubíček <ales@roubicek.name>
2025-07-31 09:45:15 +0200

    Wording

--- donut.hackercamp.cz/src/esm/admin-registrations.js
+++ donut.hackercamp.cz/src/esm/admin-registrations.js
@@ -433,7 +433,7 @@ export function registrationDetailTemplate({ detail, selectedView }) {
         </button>
       </div>
       ${ticketDetail(detail)}
-      ${when(detail.inviter, () => html`<p>Pozval ho <strong>${detail.inviter}</strong></p>`)}
+      ${when(detail.inviter, () => html`<p>Pozvání: <strong>${detail.inviter}</strong></p>`)}
       <p>Ubytování: <strong>${housing.get(detail.housing) ?? "Ještě si nevybral"}</strong></p>
       <p>Doprava: <strong>${travel.get(detail.travel) ?? "Ještě si nevybral"}</strong></p>
       ${
