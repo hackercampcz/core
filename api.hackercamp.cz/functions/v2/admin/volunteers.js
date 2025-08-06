6739652cc17b5fb0110ab708e442dfdcbf68287b
Aleš Roubíček <ales@roubicek.name>
2025-08-06 15:48:02 +0200

    Added volunteers API

---
+++ api.hackercamp.cz/functions/v2/admin/volunteers.js
@@ -0,0 +1,44 @@
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+
+async function* getRegistrations(client, year) {
+  const result = await client.send(
+    new ScanCommand({
+      TableName: "registrations",
+      ProjectionExpression:
+        "email, #y, invoice_id, paid, invoiced, #ts, volunteerArrivalDay, volunteerBar, volunteerConstruction, volunteerDriver, volunteerInfoDeskAndRegistration, volunteerSport",
+      FilterExpression: "#y = :y AND ticketType = :volunteer",
+      ExpressionAttributeNames: { "#y": "year", "#ts": "timestamp" },
+      ExpressionAttributeValues: { ":y": { N: year.toString() }, ":volunteer": { S: "volunteer" } }
+    })
+  );
+  if (result.hasOwnProperty(Symbol.asyncIterator)) {
+    for await (const page of result) {
+      yield page.Items;
+    }
+  } else {
+    yield result.Items;
+  }
+}
+
+function credentialProvider(env) {
+  return () => ({
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY
+  });
+}
+
+/**
+ * @param {EventContext<Env>} context
+ * @returns {Promise<Response>}
+ */
+export async function onRequestGet({ env, request }) {
+  const url = new URL(request.url);
+  const year = Number.parseInt(url.searchParams.get("year"));
+  const client = new DynamoDBClient({
+    region: env.AWS_REGION,
+    credentialDefaultProvider: credentialProvider(env)
+  });
+
+  const regs = await getRegistrations(client, year);
+  return Response.json(regs);
+}
