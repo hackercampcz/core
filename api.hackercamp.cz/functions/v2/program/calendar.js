74ae9f1f4b29bb267851a77926fbaaddb4b171db
Aleš Roubíček <ales@roubicek.name>
2025-08-19 09:37:11 +0200

    Use url tagged literal

--- api.hackercamp.cz/functions/v2/program/calendar.js
+++ api.hackercamp.cz/functions/v2/program/calendar.js
@@ -1,16 +1,20 @@
+function url(strings, ...values) {
+  return String.raw({ raw: strings }, ...values.map(x => x instanceof URLSearchParams ? x : encodeURIComponent(x)));
+}
+
 /**
  * @param {EventContext<Env>} context
  * @returns {Promise<Response>}
  */
 export async function onRequestGet({ env }) {
-  const calendarId = "c_afd9546ec33f93662ac0338da6d23a6b7daa6b5a1cc181afabaf6a8b11691b3a%40group.calendar.google.com";
+  const calendarId = "c_afd9546ec33f93662ac0338da6d23a6b7daa6b5a1cc181afabaf6a8b11691b3a@group.calendar.google.com";
   const time = new Date().toISOString();
   const params = new URLSearchParams({
     key: env.GOOGLE_API_KEY,
     timeMin: time,
     orderBy: "startTime"
   });
-  const resp = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?${params}`);
+  const resp = await fetch(url`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?${params}`);
   const { items } = await resp.json();
   const result = items.map(({ summary, description, start, end, extendedProperties }) => ({
     id: extendedProperties.shared.id,
