695afc941e6bc430fcb4050f0a87e71d460d93d4
Aleš Roubíček <ales@roubicek.name>
2025-08-22 14:37:44 +0200

    Cleanup NfcTron SQS handler and save vip status of the chip

--- api.hackercamp.cz/src/lambda/sqs/nfctron/handler.mjs
+++ api.hackercamp.cz/src/lambda/sqs/nfctron/handler.mjs
@@ -1,14 +1,5 @@
-import {
-  DeleteItemCommand,
-  DynamoDBClient,
-  GetItemCommand,
-  PutItemCommand,
-  ScanCommand,
-  UpdateItemCommand
-} from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
-import { attributes, mapper } from "@hackercamp/lib/attendee.js";
-import { selectKeys } from "@hackercamp/lib/object.js";
 import Rollbar from "../../rollbar.mjs";
 
 /** @typedef {import("aws-lambda").SQSEvent} SQSEvent */
@@ -16,10 +7,6 @@ import Rollbar from "../../rollbar.mjs";
 const db = new DynamoDBClient({});
 const rollbar = Rollbar.init({ lambdaName: "sqs-nfctron" });
 const { db_table_attendees } = process.env;
-
-// TODO: fetch pairing table from api/v2/nfctron
-// * get chipId by serialNumber from pairing table
-// * update attendees.nfcTronData[x].chipID
 
 async function getAttendee({ slackID, year }) {
   console.log({ event: "Get attendee", year, slackID });
@@ -45,15 +32,14 @@ function updateAttendee(attendee) {
 async function getPairingTable() {
   const resp = await fetch("https://api.hackercamp.cz/v2/nfctron");
   const data = await resp.json();
-  return new Map(data.map(x => [x.serialNumber, x.chipId]));
+  return new Map(data.map(x => [x.serialNumber, { chipID: x.chipId, vip: x.vip }]));
 }
 
-async function onAttendeeCheckIn({ key }, env) {
-  const pairingTable = await getPairingTable();
-  const attendee = await getAttendee(key);
+async function onAttendeeCheckIn({ key }) {
+  const [pairingTable, attendee] = await Promise.all([getPairingTable(), getAttendee(key)]);
   if (!attendee) return;
   for (const chip of attendee.nfcTronData.filter(x => !x.chipID)) {
-    chip.chipID = pairingTable.get(chip.serialNumber);
+    Object.assign(chip, pairingTable.get(chip.serialNumber));
   }
   await updateAttendee(attendee);
 }
@@ -61,7 +47,7 @@ async function onAttendeeCheckIn({ key }, env) {
 async function dispatchMessageByType(message) {
   switch (message.event) {
     case "check-in":
-      await onAttendeeCheckIn(message.payload, process.env);
+      await onAttendeeCheckIn(message.payload);
       break;
     default:
       throw new Error("Unknown event: " + message.event);
