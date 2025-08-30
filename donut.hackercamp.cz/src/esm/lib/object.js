389d7dd97fe8a873cd187dffb87d05c7c18ac081
Aleš Roubíček <ales@roubicek.name>
2025-08-30 08:04:04 +0200

    fix grammar

--- donut.hackercamp.cz/src/esm/lib/object.js
+++ donut.hackercamp.cz/src/esm/lib/object.js
@@ -1,7 +1,7 @@
 import { isISODateTime } from "./validation.js";
 
 /**
- * Executes a function on each of an objects own enumerable properties. The
+ * Executes a function on each of an object's own enumerable properties. The
  *  callback function will receive three arguments: the value of the current
  *  property, the name of the property, and the object being processed. This is
  *  roughly equivalent to the signature for callbacks to
@@ -18,11 +18,11 @@ export function objectForeach(obj, callback) {
 }
 
 /**
- * Walks through an object executing user defined functions at every node on the
- *  way down and back up. Functions will be given three arguments: the value
- *  of the current node, the name of the current node, and the object being
- *  being walked through. This roughly resembles the callback signature of
- *  Array.prototype.map.
+ * Walks through an object executing user-defined functions at every node on the
+ * way down and back up. Functions will be given three arguments: the value
+ * of the current node, the name of the current node, and the object being
+ * walked through. This roughly resembles the callback signature of
+ * Array.prototype.map.
  * @param {Object} obj The object to walk through.
  * @param {Function} [descentionFn = function () {return null;}] callback
  *  function to be executed at every node from the top down.
@@ -53,7 +53,7 @@ export function objectWalk(obj, descentionFn, ascentionFn) {
 }
 
 /**
- * Walks trough given object and calls new Date(value) if ISO date
+ * Walks through a given object and calls new Date(value) if ISO date
  * @param {Object} input object
  * @returns
  */
