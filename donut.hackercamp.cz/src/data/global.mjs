67e7441b0da4e40407a302e529a42cee31afe84c
Aleš Roubíček <ales@roubicek.name>
2025-08-30 07:28:29 +0200

    Extract common code to lib

--- donut.hackercamp.cz/src/data/global.mjs
+++ donut.hackercamp.cz/src/data/global.mjs
@@ -1,3 +1,6 @@
+import event from "@hackercamp/lib/event.js";
+import ticket from "@hackercamp/lib/ticket.js";
+
 function byVariant({ name, type }) {
   return x => x.type === type && x.variant === name;
 }
@@ -13,26 +16,7 @@ export default {
   program: {
     enabledSheetEdit: true
   },
-  event: {
-    name: "Hacker Camp",
-    startDate: new Date("2025-08-28T17:00:00+02:00"),
-    endDate: new Date("2025-08-31T12:00:00+02:00"),
-    get willStartSoon() {
-      const today = new Date();
-      const threeDaysBefore = new Date(this.startDate);
-      threeDaysBefore.setDate(threeDaysBefore.getDate() - 3);
-      threeDaysBefore.setHours(0, 0, 0, 0);
-      return threeDaysBefore <= today && today <= this.endDate;
-    },
-    isRegistrationOpen: true,
-    location: "Sobeňák, Starý Rožmitál 148, Rožmitál pod Třemšínem",
-    details: "Zažijte kemp pro ty, kteří chtějí lepší budoucnost."
-  },
-  ticket: {
-    nonprofit: { name: "Táborník z neziskovky", price: 3000 },
-    hacker: { name: "Hacker", price: 7000 },
-    "hacker-plus": { name: "Hacker filantrop", price: 12000 },
-    "hacker-patron": { name: "Patron Campu", price: 12000 }
-  },
+  event,
+  ticket,
   byVariant
 };
--- www.hackercamp.cz/src/data/global.mjs
+++ www.hackercamp.cz/src/data/global.mjs
@@ -1,3 +1,6 @@
+import event from "@hackercamp/lib/event.js";
+import ticket from "@hackercamp/lib/ticket.js";
+
 export default {
   meta: {
     title: "Hacker Camp",
@@ -9,25 +12,6 @@ export default {
   get currentYear() {
     return new Date().getFullYear();
   },
-  event: {
-    name: "Hacker Camp",
-    startDate: new Date("2025-08-28T17:00:00+02:00"),
-    endDate: new Date("2025-08-31T12:00:00+02:00"),
-    get willStartSoon() {
-      const today = new Date();
-      const threeDaysBefore = new Date(this.startDate);
-      threeDaysBefore.setDate(threeDaysBefore.getDate() - 3);
-      threeDaysBefore.setHours(0, 0, 0, 0);
-      return threeDaysBefore <= today && today <= this.endDate;
-    },
-    isRegistrationOpen: true,
-    location: "Sobeňák, Starý Rožmitál 148, Rožmitál pod Třemšínem",
-    details: "Zažijte kemp pro ty, kteří chtějí lepší budoucnost."
-  },
-  ticket: {
-    nonprofit: { name: "Táborník z neziskovky", price: 3000 },
-    hacker: { name: "Hacker", price: 7000 },
-    "hacker-plus": { name: "Hacker filantrop", price: 12000 },
-    "hacker-patron": { name: "Patron Campu", price: 12000 }
-  }
+  event,
+  ticket
 };
