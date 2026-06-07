71d948e6c741b6f8b82e4fb24fbb43330ea914a1
Aleš Roubíček <ales@roubicek.name>
2026-06-07 20:28:50 +0200

    Added localstorage mock

--- donut.hackercamp.cz/vitest.config.js
+++ donut.hackercamp.cz/vitest.config.js
@@ -3,5 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "happy-dom",
+    setupFiles: ['vitest-localstorage-mock'],
+    mockReset: false,
   },
 });
