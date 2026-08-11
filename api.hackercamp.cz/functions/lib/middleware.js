5c5d49e289de013a07d4389fd537b71391bb518e
Aleš Roubíček <ales@roubicek.name>
2026-08-11 15:58:21 +0200

    Allow header CORS

--- api.hackercamp.cz/functions/lib/middleware.js
+++ api.hackercamp.cz/functions/lib/middleware.js
@@ -31,6 +31,7 @@ export async function gracefulOptions(context) {
 export async function cors({ request, next }) {
   const response = await next();
   response.headers.set("Access-Control-Allow-Origin", request.headers.get("origin") ?? "*");
+  response.headers.set("Access-Control-Allow-Header", "*");
   response.headers.set("Access-Control-Max-Age", "86400");
   return response;
 }
