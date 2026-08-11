81cac533e55066f4b9692ba1a5256692a1ef132a
Aleš Roubíček <ales@roubicek.name>
2026-08-11 16:35:06 +0200

    fok me

--- api.hackercamp.cz/functions/lib/middleware.js
+++ api.hackercamp.cz/functions/lib/middleware.js
@@ -31,7 +31,7 @@ export async function gracefulOptions(context) {
 export async function cors({ request, next }) {
   const response = await next();
   response.headers.set("Access-Control-Allow-Origin", request.headers.get("origin") ?? "*");
-  response.headers.set("Access-Control-Allow-Headers", "*");
+  response.headers.set("Access-Control-Allow-Headers", request.headers.get("access-control-request-headers") ?? "*");
   response.headers.set("Access-Control-Max-Age", "86400");
   return response;
 }
