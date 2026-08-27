3daa75fe9187d01a05338c1b35f531264a116b26
Aleš Roubíček <ales@roubicek.name>
2026-08-27 11:32:19 +0200

    Reorder middleware

--- api.hackercamp.cz/functions/v2/admin/_middleware.js
+++ api.hackercamp.cz/functions/v2/admin/_middleware.js
@@ -1,8 +1,8 @@
 import { allowCredentials, allowMethods, gracefulOptions, roleAuthorization } from "#lib/middleware.js";
 
 export const onRequest = [
-  allowCredentials,
   allowMethods(["GET", "POST", "OPTIONS"]),
+  allowCredentials,
   gracefulOptions,
   roleAuthorization("admin")
 ];
